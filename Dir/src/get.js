4d2ff1b0b626adf4300e2708c421588a4975c49b
Marcin Polak <mapoart@gmail.com>
2020-03-08 02:52:04 +0100

    Dir using new nexss/lib

--- Dir/src/get.js
+++ Dir/src/get.js
@@ -6,7 +6,23 @@ const {
 
 const NexssIn = require(`${process.env.NEXSS_PACKAGES_PATH}/Nexss/Lib/NexssIn.js`);
 
-console.log(NexssIn());
+// console.log(JSON.stringify());
+
+let NexssStdout = NexssIn();
+
+if (NexssStdout.FSGetPath) {
+  Field_Value_1 = NexssStdout.FSGetPath;
+  delete NexssStdout.FSGetPath;
+} else {
+  if (!NexssStdout.nxsIn) {
+    nxsError(
+      "You need to provide path. Or with parameter as Nexss FS/Get param OR nxsIn OR --FSGetPath"
+    );
+    process.exit(0);
+  } else {
+    NexssStdout.nxsIn[0];
+  }
+}
 
 // Modify data
 //NexssStdout.NodeJSOutput = `Hello from NodeJS! ${process.version}`;
@@ -14,8 +30,8 @@ console.log(NexssIn());
 //   if (NexssStdout.nxsOutAs) {
 //     fieldOut = NexssStdout.nxsOutAs;
 //   }
-
-//   NexssStdout[fieldOut] = getFiles(Field_Value_1);
-//   // STDOUT
-//   process.stdout.write(JSON.stringify(NexssStdout));
-// });
+process.chdir(NexssStdout.cwd);
+NexssStdout[NexssStdout.resultField_1] = getFiles(NexssStdout.nxsIn[0]);
+delete NexssStdout.resultField_1;
+delete NexssStdout.nxsIn;
+process.stdout.write(JSON.stringify(NexssStdout));
