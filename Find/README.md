8c7afef1b2c471a6cf3bdf275ec4ba5b7d777b91
Marcin Polak <mapoart@gmail.com>
2020-06-08 16:15:08 +0200

    more functions for the find

--- Find/README.md
+++ Find/README.md
@@ -1,11 +1,27 @@
 # Nexss PROGRAMMER 2.0
 
-find files. This package uses great ripgrep !
+find files, with content search.
+
+## Parameters
+
+- nexss FS/Find pattern1 pattern2 paternX folder --param1 --param2
+
+- **pathMatch** (can be multiple) - will find filenames/path by regexp eg `--pathMatch=_.txt` `--pathMatch=_.rc`
+  You can use `!` to negate: eg --inFiles=!\*2020.txt
+- **inTypes** (can be multiple) - will search by types (if type is wrong will display list of available types - are the same as -- ripgrep --type-list)
+- **inZip** - search in compressed files
+- **caseSensitive** - enable case sensitive search
 
 ## Examples
 
 ```sh
-nexss fs/find 'invoice' ./invoices # finds files which contains word 'invoice'
+nexss FS/Find invoice ./invoices # finds files which contains word 'invoice' in the content (second or last parameter is where to look)
+nexss FS/Find invoice faktura ./invoices # here will find faktura and invoice in the file content in the folder 'invoices'
+nexss FS/Find --pathMatch=*2020.txt # find only files without checking the content
+nexss FS/Find faktura --pathMatch=*2020.txt --inTypes=txt --caseSensitive # find in txt files, using caseSensitive, path match *2020.txts
+nexss FS/Find faktura --inTypes=txt --pathMatch=*something* --pathMatch=!*8* # will match path something, but not with number 8
 ```
 
 ## More info
+
+This package uses great ripgrep !
--- Find/find.js
+++ Find/find.js
@@ -1,43 +1,128 @@
 const {
   nxsError,
   nxsInfo,
+  nxsDebug,
 } = require(`${process.env.NEXSS_PACKAGES_PATH}/Nexss/Lib/NexssLog.js`);
 
 const NexssIn = require(`${process.env.NEXSS_PACKAGES_PATH}/Nexss/Lib/NexssIn.js`);
 let NexssStdout = NexssIn();
 
+function paramRecognizer(InParam, resultParam, NexssStdout) {
+  let r;
+  if (NexssStdout[InParam]) {
+    if (Array.isArray(NexssStdout[InParam])) {
+      r = NexssStdout[InParam].reduce(
+        (acc, g) => acc.concat([resultParam, g]),
+        []
+      );
+    } else {
+      r = [resultParam, NexssStdout[InParam]];
+    }
+    delete NexssStdout[InParam];
+    return r;
+  }
+}
+
 process.chdir(NexssStdout.cwd);
 
-if (!NexssStdout.nxsIn) {
-  nxsError("Enter phrase to search eg Nexss FS/Find invoice");
-  process.exit(1);
-}
+// if (!NexssStdout.nxsIn) {
+//   nxsError("Enter phrase to search eg Nexss FS/Find invoice");
+//   process.exit(1);
+// }
 
 const { spawnSync } = require("child_process");
 const { normalize } = require("path");
 // execSync("rg " + NexssStdout.nxsIn.join(" "));
 // --after-context=11 --before-context=10
-var rg = spawnSync("rg", [...NexssStdout.nxsIn, "-i", "--json"], {
+
+// Parse parameters
+
+let params = [];
+let where = ".";
+if (!NexssStdout.nxsIn) {
+  params = ["--files"];
+} else {
+  if (NexssStdout.nxsIn.length > 1) {
+    where = NexssStdout.nxsIn.pop();
+  }
+  params = NexssStdout.nxsIn.reduce((acc, e) => acc.concat(["-e", e]), []);
+  params.push("--json");
+}
+
+let globs = paramRecognizer("pathMatch", "-g", NexssStdout);
+if (globs) {
+  params = params.concat(globs);
+  delete NexssStdout.pathMatch;
+}
+
+let types = paramRecognizer("inTypes", "-t", NexssStdout);
+if (types) {
+  params = params.concat(types);
+  delete NexssStdout.inTypes;
+}
+
+let notInTypes = paramRecognizer("notInTypes", "-T", NexssStdout);
+if (notInTypes) {
+  params = params.concat(notInTypes);
+  delete NexssStdout.notInTypes;
+}
+
+if (!NexssStdout.caseSensitive) {
+  params = params.concat("-i");
+  delete NexssStdout.caseSensitive;
+}
+
+if (NexssStdout.inZip) {
+  params = params.concat("--search-zip");
+  delete NexssStdout.inZip;
+}
+
+if (NexssStdout.inPath) {
+  params = params.concat(NexssStdout.inPath);
+  delete NexssStdout.inPath;
+} else {
+  // params = params.concat(".");
+}
+
+if (NexssStdout.debug || NexssStdout.nxsLearning) {
+  nxsInfo("rg " + params.join(" "));
+}
+
+var rg = spawnSync("rg", [...params, where], {
   encoding: "utf8",
 });
 
+const r = rg.stdout.trim();
 
+if (r) {
+  result = r.split("\n");
 
-// nxsInfo(`Run rg ${NexssStdout.nxsIn}`);
+  if (NexssStdout.nxsIn) {
+    result = result
+      .filter((match) => {
+        let data = JSON.parse(match);
+        if (data.type === "match") {
+          return match;
+        }
+      })
+      .map((a) => JSON.parse(a))
+      .map((e) => normalize(e.data.path.text));
+  }
 
-NexssStdout.nxsOut = rg.stdout
-  .trim()
-  .split("\n")
-  .filter((match) => {
-    let data = JSON.parse(match);
-    if (data.type === "match") {
-      return match;
-    }
-  })
-  .map((a) => JSON.parse(a))
-  .map((e) => normalize(e.data.path.text));
+  NexssStdout[NexssStdout.resultField_1] = result;
+} else {
+  nxsError("There was an error:");
+  if (rg.stderr.startsWith("unrecognized file type:")) {
+    nxsInfo(require("child_process").execSync("rg --type-list").toString());
+  }
+
+  nxsError(rg.stderr);
+  nxsError("Above is the list of available types. (Ripgrep)");
+  process.exit(1);
+}
+
+NexssStdout.FSFindTotal = result ? result.length : 0;
 
 delete NexssStdout.nxsIn;
 delete NexssStdout.resultField_1;
-delete NexssStdout.storageFilename;
 process.stdout.write(JSON.stringify(NexssStdout));
